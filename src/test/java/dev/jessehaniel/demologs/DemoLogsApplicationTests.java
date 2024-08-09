13590208308f65e9a2ea7a5ea0f6847e786c1a5a
Jessé Haniel <jesse.haniel@tjpb.jus.br>
2024-08-09 10:15:16 -0300

    first commit

---
+++ src/test/java/dev/jessehaniel/demologs/DemoLogsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jessehaniel.demologs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
